2e5085f6fd41cdb4dbc5f654e4246a0dc8446b08
Wamariu <mirungu003@gmail.com>
2023-03-17 09:30:35 -0700

    part 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char charactertype;
+	int integertype;
+	long int longintegertype;
+	long long int longlongintegertype;
+	float floattype;
+	
+	printf("Size of char: %\ld byte\n",(charactertype));
+	printf("Size of int: %\ld byte\n",(integertype));
+	printf("Size of long int: %\ld byte\n",(longintegertype))
+	printf("Size of long long int: %\ld byte\n",(longlonginteger))
+	printf("Size of float %\ld byte\n",(floattype))
+}
