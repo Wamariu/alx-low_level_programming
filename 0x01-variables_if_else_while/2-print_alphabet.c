bbfac2e7b6da4453120930c22b715c7096afc4f7
Wamariu <mirungu003@gmail.com>
2023-03-18 09:40:36 -0700

    c02

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Description: use putchar to print alphabet letters in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = a;
+
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
