92ec19a9223b4019eae9b9c39c207822a26dbc6b
Wamariu <mirungu003@gmail.com>
2023-03-21 11:19:07 -0700

    f2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	{
+		int i = 0;
+
+		while (i < 11)
+		i++
+	}
+	_putchar('\n')
+}	
