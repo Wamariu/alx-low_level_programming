7a57d136250fe2abbf1355d64a50c895c50f2a8f
Wamariu <mirungu003@gmail.com>
2023-03-21 12:10:53 -0700

    f5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ * @n: number to be checked
+ *
+ * Return: 1 if greater, 0 if zero , -1 if less than
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		((return (1)), printf("+");
+	else if (n == 0)
+		((return (0)), printf("0");
+	else
+		((return (-1)), printf("-");
+}
