5c4a4249b98df498e87c15047d29647d036c1d31
Wamariu <mirungu003@gmail.com>
2023-03-21 12:53:44 -0700

    last straw

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,14 @@
 #include "holberton.h"
 /**
  * _abs - Computes the absolute value of an integer.
- * @n: The integer to be computed.
+ * @int: The integer to be computed.
  *
  * Return: The absolute value of the integer
  */
-int _abs(int n)
+int _abs(int)
 {
+	int n;
+
 	if (n >= 0)
 		return (n);
 	else
