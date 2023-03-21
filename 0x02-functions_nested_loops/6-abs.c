dfe1022fc91372a5720ddba8f6c8da993f43079c
Wamariu <mirungu003@gmail.com>
2023-03-21 12:39:10 -0700

    f6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * _abs - computes absolute values of an integer
+ * @int: integer
+ *
+ * Return: The absolute value of the integer
+ */
+int _abs(int);
+{
+	int x;
+
+	if (x >= 0)
+		return (n);
+	else
+		return (-n);
+}
