5d5c19f6bfe7c7c4245ffac338a3ceb4c0f18862
Wamariu <mirungu003@gmail.com>
2023-03-23 11:14:51 -0700

    mine

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: character to be multiplied
+ * @b: character to be multiplied
+ *
+ * Return: The result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (int a * int b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c == '0'; c <= '9'; c++)
+		_putchar(c);
+
+	_putchar("\n");
+
+	return (0);
+}
