1dc7967f6cbb553c318be990705d51167c112bf1
Wamariu <mirungu003@gmail.com>
2023-03-23 11:36:08 -0700

    d

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,10 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c = '0'; c <= '9'; c++)
-		_putchar((c % 10) + '0');
+	for (num = '0'; num <= '9'; num++)
+		_putchar((num % 10) + '0');
 
 	_putchar('\n');
 }
