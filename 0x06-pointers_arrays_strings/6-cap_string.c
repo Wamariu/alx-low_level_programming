12094f842f6ea4561c67c140ad743dad91ec5df7
Wamariu <mirungu003@gmail.com>
2023-03-30 09:52:16 -0700

    siuu

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -30,7 +30,7 @@ char *cap_string(char *str)
 			else if (str[i] > 64 && str[i] < 91)
 				trigger = 0;
 			else if (str[i] > 47 && str[i] < 58)
-				triger = 0;
+				trigger = 0;
 		}
 	}
 	return (str);
