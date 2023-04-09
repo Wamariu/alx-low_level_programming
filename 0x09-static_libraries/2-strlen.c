f57bde136cf5273696be1b2ef243f40a2a49b7c6
Wamariu <mirungu003@gmail.com>
2023-04-09 06:32:42 -0700

    correction

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be counted
+ *
+ * Return: string length number
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+		;
+
+	return (c);
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - used to check for alphabetic character
+ * @c: to be checked
+ *
+ * Return: 1 if alphabetic character 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+
