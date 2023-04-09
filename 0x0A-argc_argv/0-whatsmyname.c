824a4bffe9682d020b3533b40cffc03e5ec553b9
Wamariu <mirungu003@gmail.com>
2023-04-09 09:58:09 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print the name of program
+ * @argc: arguement count
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
