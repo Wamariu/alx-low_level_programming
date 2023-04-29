3990699d076f23c4330d7664ae3c542d8a3b1cfe
Wamariu <mirungu003@gmail.com>
2023-04-29 12:03:24 -0700

    02

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -23,3 +23,5 @@ void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+#endif
