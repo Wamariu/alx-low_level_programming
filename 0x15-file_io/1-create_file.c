cd98dbc130e39a271493fb2724b711f80b854c96
Wamariu <mirungu003@gmail.com>
2023-05-11 04:33:04 -0700

    5

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	0 = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
